a535048740197b830a200a4223284cfa65c41e03
Stephen Ball <sdball@gmail.com>
2012-02-21 22:02:24 -0500

    spec for actual data result

--- spec/lib/periodic_table_spec.rb
+++ spec/lib/periodic_table_spec.rb
@@ -4,5 +4,7 @@ describe PeriodicTable do
   it "should return data for a named element" do
     element_data = PeriodicTable.lookup('oxygen')
     element_data.should_not be_nil
+    element_data.symbol.should == 'O'
+    element_data.atomic_weight.should == '15.9994'
   end
 end

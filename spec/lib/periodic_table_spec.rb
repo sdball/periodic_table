328abfdcb7dda8b1bf7f8813c39ee5305a7c43c1
Stephen Ball <sdball@gmail.com>
2012-02-29 19:29:06 -0500

    periodic_table vcr cassette

--- spec/lib/periodic_table_spec.rb
+++ spec/lib/periodic_table_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe PeriodicTable do
+  use_vcr_cassette 'periodic_table'
   it "should return data for a named element" do
     element_data = PeriodicTable.lookup('oxygen')
     element_data.should_not be_nil

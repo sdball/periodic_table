de1d13816639306e4d67b4f91a260e5b4a4b581a
Stephen Ball <sdball@gmail.com>
2012-02-29 19:28:47 -0500

    vcr config and rspec macros

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,11 @@
 require 'periodic_table'
+require 'vcr'
+
+VCR.config do |c|
+  c.cassette_library_dir = 'spec/support/cassettes'
+  c.stub_with :fakeweb
+end
+
+RSpec.configure do |config|
+  config.extend VCR::RSpec::Macros
+end
